b23fa0195d4d3aa88193ad1433beec782b398395
fatime4 <102423500+fatime4@users.noreply.github.com>
2022-07-05 14:44:03 +0300

    c1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const allUlEl = document.querySelectorAll("li.item").length;
+
+console.log(`Number of categories: ${allUlEl}`);
+
+// console.log(`Category: ${}`);
+// console.log(`Elements: ${}`);
